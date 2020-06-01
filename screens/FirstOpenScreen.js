5e107ac697e498273c8d7fd23b9e050589a28a43
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-06-01 13:23:05 -0400

    Style first open page and change splash background to be purple

--- screens/FirstOpenScreen.js
+++ screens/FirstOpenScreen.js
@@ -1,6 +1,6 @@
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
-import { AsyncStorage, View, Image, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { AsyncStorage, View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import { Link } from '@react-navigation/native';
 
 import { NanumText } from '../components/StyledText';
@@ -10,12 +10,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  image: {
-    width: 350,
-    height: 350,
+  headerText: {
+    fontFamily: 'jua',
+    fontSize: 50,
+    color: '#6674DE',
     marginLeft: 'auto',
     marginRight: 'auto',
-    marginBottom: '50%'
+    marginTop: '15%',
+  },
+  image: {
+    width: 250,
+    height: 250,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginBottom: '50%',
   },
   signUpBtn: {
     backgroundColor: '#6674DE',
@@ -29,7 +37,29 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     color: 'white',
-    marginTop: 7.5,
+    marginTop: 'auto',
+    marginBottom: 'auto',
+  },
+  loginContainer: {
+    marginTop: '5%',
+  },
+  logInBtn: {
+    backgroundColor: 'white',
+    width: '30%',
+    height: '5%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: '5%',
+    borderRadius: 5,
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  loginText: {
+    fontSize: 18,
+    color: '#6674DE',
+    textDecorationLine: 'underline',
+    marginLeft: 'auto',
+    marginRight: 'auto',
   }
 });
 
@@ -38,14 +68,22 @@ export default function FirstOpenScreen() {
     console.log('Sign Up for Just My Size');
   };
 
+  const logIn = () => {
+    console.log('Login to Just My Size');
+  };
+
   return (
     <View style={styles.container}>
-      <Image source={require('../assets/images/jms_splash.png')} style={styles.image} />
+      <Text style={styles.headerText}>JUST MY SIZE</Text>
+      <Image source={require('../assets/images/jms_icon.png')} style={styles.image} />
       <TouchableOpacity onPress={signUp} style={styles.signUpBtn}>
-        <Text style={styles.signUpText}>SIGN UP</Text>
+        <NanumText style={styles.signUpText}>SIGN UP</NanumText>
       </TouchableOpacity>
-      {/* Need to set login path */}
-      <Link to="/login">Login</Link>
+      <View style={styles.loginContainer}>
+      <TouchableOpacity onPress={logIn} style={styles.logInBtn}>
+        <NanumText style={styles.loginText}>Login</NanumText>
+      </TouchableOpacity>
+      </View>
     </View>
   );
 };
