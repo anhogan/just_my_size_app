da60b41b9378d09e6ab75dcaa2d5d5f35c33dd5f
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-06-03 09:33:49 -0400

    Setup custom headers if display name present

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,11 +1,13 @@
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
+
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
 import { NanumText } from '../components/StyledText';
 
 export default function Profile() {
+
   return (
     <View style={styles.container}>
       <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
