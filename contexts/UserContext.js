bfcfb2f1e2f13241645a64168d7885d1d577c791
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-06-06 14:01:04 -0400

    Change User Provider to be a class component - renders with unfound props variable error

--- contexts/UserContext.js
+++ contexts/UserContext.js
@@ -4,15 +4,35 @@ export const UserContext = React.createContext();
 
 export const UserConsumer = UserContext.Consumer;
 
-export function UserProvider() {
-  const [newUser, setNewUser] = React.useState(false);
+// Does this have to be a class component? Rendering as a function returns a blank screen
+// Not sure what to substitute for this.props.children for a function?
+// Currently, this renders a can't find variable: toggleNewUser error on SignUp Screen ... probably for all but that one was up before I changed it
+const initialState = {
+  newUser: false
+};
 
-  const toggleNewUser = () => {
-    newUser ? setNewUser(false) : setNewUser(true);
+export class UserProvider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
   };
 
-  return (
-    <UserContext.Provider value={{ toggleNewUser: toggleNewUser }}>
-    </UserContext.Provider>
-  )
+  toggleNewUser = () => {
+    this.state.newUser ? this.setState({ newUser: false }) : this.setState({ newUser: true })
+  };
+
+  // const [newUser, setNewUser] = React.useState(false);
+
+  // const toggleNewUser = () => {
+  //   newUser ? setNewUser(false) : setNewUser(true);
+  // };
+
+  render() {
+    return (
+      <UserContext.Provider value={{ toggleNewUser: toggleNewUser }}>
+        {/* What goes here? {this.props.children} for a class component? */}
+        {this.props.children}
+      </UserContext.Provider>
+    )
+  }
 }
